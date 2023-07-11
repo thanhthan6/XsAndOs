2816ca5895e71f7175dce0226f91df2ac156a4f5
thanhthan6 <xlthanhthan@gmail.com>
2023-07-12 06:14:48 +0700

    Initial Commit

---
+++ XsAndOs/XsAndOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsAndOsApp.swift
+//  XsAndOs
+//
+//  Created by Macbook on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct XsAndOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
